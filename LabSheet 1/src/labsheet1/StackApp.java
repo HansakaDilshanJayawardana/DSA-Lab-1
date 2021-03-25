682f9aa6c4e1a6a5d1e6bb06622981ac6f920a4b
HansakaDilshanJayawardana <hansaka345@gmail.com>
2021-03-26 02:09:55 +0530

    DSA LabSheet 1

--- LabSheet 1/src/labsheet1/StackApp.java
+++ LabSheet 1/src/labsheet1/StackApp.java
@@ -1,5 +1,7 @@
 package labsheet1;
 
+import java.util.Scanner;
+
 public class StackApp {
 
 	public static void main(String[] args) {
@@ -12,9 +14,38 @@ public class StackApp {
 		st.push('d');
 		
 		while (!st.isEmpty()) {
-			System.out.println(st.pop());
+			System.out.println(st.pop());//deleted items output
 		}
-
+		
+		//part c
+		System.out.println("Enter an expression ");//prompt message
+		Scanner sc = new Scanner(System.in);//create scanner object
+		String str = sc.nextLine();//catch the values
+		
+		int len = str.length();//length of the user input string
+		
+		StackX stackX = new StackX(len);//create a stack object using that length
+		
+		//go upto the length of that string 
+		for (int x = 0; x < len; x++) {
+			char ch = str.charAt(x);//check the character one by one
+			//if character is opening bracket
+			if (ch == '(') {
+				stackX.push(ch);//insert to the stack
+			} else if (ch == ')') { //if character is closing bracket 
+				char ch2 = stackX.pop();//remove value from the stack
+				//if removed value is not a opening bracket
+				if (ch2 != '(') {
+					System.out.println("Invalid String");//prompt message
+					return;
+				}
+			}
+		}
+		//whether the stack is empty
+		if (stackX.isEmpty())
+			System.out.println("Valid String");
+		else
+			System.out.println("Invalid String");
 	}
 
 }
